7bf3b8e34895e7f66b12d95d9b2543e9e169078c
Steve Avon <savon40@gmail.com>
2020-02-27 18:22:07 -0500

    initial commit with some backend and frontend changes

---
+++ client/src/components/Checkout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Checkout extends React.Component {
+    render() {
+        console.log('here');
+        return (
+            <div>Checkout</div>
+        )
+    }
+}
+
+export default Checkout;
