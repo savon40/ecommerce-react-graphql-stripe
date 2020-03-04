defc101e41579deff7625dfa6ae644d5726f01ab
Steve Avon <savon40@gmail.com>
2020-03-04 15:22:49 -0500

    finished section 8 the user cart section

--- client/src/components/Brews.js
+++ client/src/components/Brews.js
@@ -1,13 +1,17 @@
 import React from "react";
 import Strapi from "strapi-sdk-javascript/build/main";
-import { Box, Heading, Text, Image, Card, Button } from "gestalt";
+// prettier-ignore
+import { Box, Heading, Text, Image, Card, Button, Mask, IconButton } from "gestalt";
+import { calculatePrice, setCart, getCart } from "../utils";
+import { Link } from "react-router-dom";
 const apiUrl = process.env.API_URL || "http://localhost:1337";
 const strapi = new Strapi(apiUrl);
 
 class Brews extends React.Component {
   state = {
     brews: [],
-    brand: ""
+    brand: "",
+    cartItems: []
   };
 
   async componentDidMount() {
@@ -33,15 +37,41 @@ class Brews extends React.Component {
       });
       this.setState({
         brews: response.data.brand.brews,
-        brand: response.data.brand.name
+        brand: response.data.brand.name,
+        cartItems: getCart()
       });
     } catch (err) {
       console.error(err);
     }
   }
 
+  addToCart = brew => {
+    const alreadyInCart = this.state.cartItems.findIndex(
+      item => item._id === brew._id
+    );
+
+    if (alreadyInCart === -1) {
+      const updatedItems = this.state.cartItems.concat({
+        ...brew,
+        quantity: 1
+      });
+      this.setState({ cartItems: updatedItems }, () => setCart(updatedItems));
+    } else {
+      const updatedItems = [...this.state.cartItems];
+      updatedItems[alreadyInCart].quantity += 1;
+      this.setState({ cartItems: updatedItems }, () => setCart(updatedItems));
+    }
+  };
+
+  deleteItemFromCart = itemToDeleteId => {
+    const filteredItems = this.state.cartItems.filter(
+      item => item._id !== itemToDeleteId
+    );
+    this.setState({ cartItems: filteredItems }, () => setCart(filteredItems));
+  };
+
   render() {
-    const { brand, brews } = this.state;
+    const { brand, brews, cartItems } = this.state;
 
     return (
       <Box
@@ -49,6 +79,11 @@ class Brews extends React.Component {
         display="flex"
         justifyContent="center"
         alignItems="start"
+        dangerouslySetInlineStyle={{
+          __style: {
+            flexWrap: "wrap-reverse"
+          }
+        }}
       >
         {/* Brews Section */}
         <Box display="flex" direction="column" alignItems="center">
@@ -99,7 +134,11 @@ class Brews extends React.Component {
                     <Text color="orchid">${brew.price}</Text>
                     <Box marginTop={2}>
                       <Text bold size="xl">
-                        <Button color="blue" text="Add to Cart" />
+                        <Button
+                          onClick={() => this.addToCart(brew)}
+                          color="blue"
+                          text="Add to Cart"
+                        />
                       </Text>
                     </Box>
                   </Box>
@@ -107,6 +146,60 @@ class Brews extends React.Component {
               </Box>
             ))}
           </Box>
+        </Box>
+
+        {/* User Cart */}
+        <Box alignSelf="end" marginTop={2} marginLeft={8}>
+          <Mask shape="rounded" wash>
+            <Box
+              display="flex"
+              direction="column"
+              alignItems="center"
+              padding={2}
+            >
+              {/* User Cart Heading */}
+              <Heading align="center" size="sm">
+                Your Cart
+              </Heading>
+              <Text color="gray" italic>
+                {cartItems.length} items selected
+              </Text>
+
+              {/* Cart Items */}
+              {cartItems.map(item => (
+                <Box key={item._id} display="flex" alignItems="center">
+                  <Text>
+                    {item.name} x {item.quantity} - $
+                    {(item.quantity * item.price).toFixed(2)}
+                  </Text>
+                  <IconButton
+                    accessibilityLabel="Delete Item"
+                    icon="cancel"
+                    size="sm"
+                    iconColor="red"
+                    onClick={() => this.deleteItemFromCart(item._id)}
+                  />
+                </Box>
+              ))}
+
+              <Box
+                display="flex"
+                alignItems="center"
+                justifyContent="center"
+                direction="column"
+              >
+                <Box margin={2}>
+                  {cartItems.length === 0 && (
+                    <Text color="red">Please select some items</Text>
+                  )}
+                </Box>
+                <Text size="lg">Total: {calculatePrice(cartItems)}</Text>
+                <Text>
+                  <Link to="/checkout">Checkout</Link>
+                </Text>
+              </Box>
+            </Box>
+          </Mask>
         </Box>
       </Box>
     );
--- client/src/index.js
+++ client/src/index.js
@@ -25,6 +25,5 @@ const Root = () => (
     </Router>
 )
 
-
 ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
