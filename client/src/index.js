3337ca00695f7514250af08bda1bdc9eb9861d89
Steve Avon <savon40@gmail.com>
2020-03-04 22:44:30 -0500

    privateroute for checkout

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import 'gestalt/dist/gestalt.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
@@ -9,6 +9,18 @@ import Signup from './components/Signup';
 import Checkout from './components/Checkout';
 import Brews from './components/Brews';
 import Navbar from './components/Navbar';
+import {getToken} from './utils/index';
+
+//redirect to signin if this is a route that unathenticated users should not visit
+const PrivateRoute = ({component: Component, ...rest}) => (
+    <Route {...rest} render={props => (
+        getToken() !== null ? 
+            <Component {...props} /> : <Redirect to ={{ 
+                pathname: '/signin',
+                state: {from: props.location}
+            }}/>
+    )} />
+)
 
 const Root = () => (
     <Router>
@@ -18,7 +30,7 @@ const Root = () => (
                 <Route component={App} exact path="/" />
                 <Route component={Signin} path="/signin" />
                 <Route component={Signup} path="/signup" />
-                <Route component={Checkout} path="/checkout" />
+                <PrivateRoute component={Checkout} path="/checkout" />
                 <Route component={Brews} path="/:brandId" />
             </Switch>
         </React.Fragment>
